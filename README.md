fa253d4ad1718baba3582c950206d1c5363bf600
Jean-Philippe Sirois <jp@veksen.net>
2019-05-08 14:47:13 -0400

    rewrite to TS using TSDX, replace Hammerjs with React-Swipeable

--- README.md
+++ README.md
@@ -9,18 +9,30 @@ yarn add better-react-swiper
 
 ```js
 // import from NPM
-import Swiper from "better-react-swiper";
+import Swiper from 'better-react-swiper';
 ```
 
 ```jsx
 // use within your component
-<div className="wrapper" style={{ margin: "60px" }}>
+<div className="wrapper" style={{ margin: '60px' }}>
   <Swiper
     items={[
-      <img src="https://picsum.photos/300/200?image=1084" style={{ width: "100%" }} />,
-      <img src="https://picsum.photos/300/200?image=1081" style={{ width: "100%" }} />,
-      <img src="https://picsum.photos/300/200?image=1070" style={{ width: "100%" }} />,
-      <img src="https://picsum.photos/300/200?image=1050" style={{ width: "100%" }} />
+      <img
+        src="https://picsum.photos/300/200?image=1084"
+        style={{ width: '100%' }}
+      />,
+      <img
+        src="https://picsum.photos/300/200?image=1081"
+        style={{ width: '100%' }}
+      />,
+      <img
+        src="https://picsum.photos/300/200?image=1070"
+        style={{ width: '100%' }}
+      />,
+      <img
+        src="https://picsum.photos/300/200?image=1050"
+        style={{ width: '100%' }}
+      />,
     ]}
   />
 </div>
---
+++ src/index.tsx
@@ -0,0 +1,199 @@
+import React, { useState } from 'react';
+import ReactResizeDetector from 'react-resize-detector';
+import { EventData, Swipeable, SwipeableOptions } from 'react-swipeable';
+
+import {
+  ArrowLeft,
+  ArrowRight,
+  Item,
+  SwiperCanvas,
+  SwiperWrapper,
+} from './styles';
+
+const MEDIA_MAX_XS = 576;
+const MEDIA_MAX_SM = 767;
+
+interface SwiperProps {
+  items: any[];
+  itemsWide?: number;
+  infinity?: boolean;
+  canvasClassName?: string;
+  canvasStyle?: React.CSSProperties;
+  arrowClassName?: string;
+  arrowStyle?: React.CSSProperties;
+  style?: React.CSSProperties;
+}
+
+export const Swiper = ({
+  items = [],
+  itemsWide = 3,
+  infinity = false,
+  canvasClassName,
+  canvasStyle,
+  arrowClassName,
+  arrowStyle,
+  style,
+}: SwiperProps): JSX.Element => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [slideOffset, setSlideOffset] = useState<number>(0);
+
+  // TODO: there has to be a better way...
+  const [lastSwipe, setLastSwipe] = useState<number | null>(null);
+  const [width, setWidth] = useState<number>(0);
+
+  const computeMedia = () => {
+    if (width <= MEDIA_MAX_XS) {
+      return 'xs';
+    }
+    if (width <= MEDIA_MAX_SM) {
+      return 'sm';
+    }
+    return 'md';
+  };
+
+  const computeItemWidth = () => {
+    return width <= MEDIA_MAX_SM ? 1 : itemsWide;
+  };
+
+  const canGoToPrevious = () => {
+    return (infinity && items.length > 1) || currentIndex !== 0;
+  };
+
+  const canGoToNext = () => {
+    const computedWide = computeItemWidth();
+
+    return (
+      (infinity && items.length > 1) ||
+      currentIndex < items.length - computedWide
+    );
+  };
+
+  const goToPrevious = () => {
+    if (!canGoToPrevious()) {
+      return;
+    }
+
+    const computedWide = computeItemWidth();
+
+    const steps = currentIndex === 0 ? computedWide : 1;
+    const prev = (items.length + currentIndex - steps) % items.length;
+
+    setCurrentIndex(prev);
+  };
+
+  const goToNext = () => {
+    if (!canGoToNext()) {
+      return;
+    }
+
+    const computedWide = computeItemWidth();
+
+    const steps = items.length - currentIndex > computedWide ? 1 : computedWide;
+    const next = (items.length + currentIndex + steps) % items.length;
+
+    setCurrentIndex(next);
+  };
+
+  const resetSwipe = () => {
+    const now = new Date().getTime();
+    setSlideOffset(0);
+    setLastSwipe(now);
+  };
+
+  const onSwiping = (e: EventData) => {
+    const now = new Date().getTime();
+
+    if (!width) {
+      return;
+    }
+
+    if (lastSwipe && now - lastSwipe < 250) {
+      return;
+    }
+
+    const draggedPercent = (e.deltaX * 2) / width;
+
+    setSlideOffset(draggedPercent * 100);
+
+    if (draggedPercent < -0.3333) {
+      resetSwipe();
+      goToPrevious();
+      return;
+    }
+
+    if (draggedPercent > 0.3333) {
+      resetSwipe();
+      goToNext();
+      return;
+    }
+  };
+
+  const onSwipeEnd = () => {
+    setSlideOffset(0);
+  };
+
+  const onResize = (w: number) => {
+    setWidth(w);
+    resetSwipe();
+  };
+
+  const swipeConfig: SwipeableOptions = {
+    trackTouch: true,
+    trackMouse: true,
+  };
+
+  const hideArrows = items.length <= itemsWide;
+
+  return (
+    <ReactResizeDetector handleWidth onResize={onResize}>
+      <SwiperWrapper style={style} media={computeMedia()}>
+        {!hideArrows && (
+          <ArrowLeft
+            data-testid="prev"
+            faded={!canGoToPrevious()}
+            onClick={goToPrevious}
+            className={arrowClassName}
+            style={arrowStyle}
+          >
+            ◀
+          </ArrowLeft>
+        )}
+        <Swipeable
+          onSwiping={eventData => onSwiping(eventData)}
+          onSwiped={onSwipeEnd}
+          {...swipeConfig}
+        >
+          <SwiperCanvas className={canvasClassName} style={canvasStyle}>
+            {items.map((item, i) => (
+              <Item
+                key={i}
+                itemsWide={computeItemWidth()}
+                currentIndex={currentIndex}
+                data-testid="item"
+                style={{
+                  left: `-${(currentIndex * 100) / computeItemWidth() +
+                    slideOffset}%`,
+                }}
+              >
+                {item}
+              </Item>
+            ))}
+          </SwiperCanvas>
+        </Swipeable>
+        {!hideArrows && (
+          <ArrowRight
+            data-testid="next"
+            faded={!canGoToNext()}
+            onClick={goToNext}
+            className={arrowClassName}
+            style={arrowStyle}
+          >
+            ▶
+          </ArrowRight>
+        )}
+      </SwiperWrapper>
+    </ReactResizeDetector>
+  );
+};
+
+export default Swiper;
