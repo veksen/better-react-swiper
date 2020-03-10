91c21ee60ac9ed5e31d6bae9cb896d01abcb38ea
Jean-Philippe Sirois <jp@veksen.net>
2020-03-10 04:50:45 -0400

    feat: convert ascii arrows to svg

---
+++ src/arrow.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Arrow = (props: any) => (
+  <svg {...props} viewBox="0 0 42 42">
+    <path d="M36.07 20.18l-29-20A1 1 0 005.5 1v40a1 1 0 001.57.82l29-20a1 1 0 000-1.64z" />
+  </svg>
+);
