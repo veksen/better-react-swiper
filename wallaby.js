aaf84bb5a72bb36801ed9058bf2a9627f49f72ac
Jean-Philippe Sirois <jp@veksen.net>
2020-03-10 22:14:09 -0400

    chore: update wallaby.js config to work with codebase

--- wallaby.js
+++ wallaby.js
@@ -1,21 +1,25 @@
 module.exports = function configWallaby(wallaby) {
   return {
     files: [
-      { pattern: "src/**/*.js*", load: true },
-      { pattern: "src/**/*.test.js*", ignore: true }
+      { pattern: 'src/**/*.ts?(x)', load: true },
+      { pattern: 'test/**/*.test.ts?(x)', ignore: true },
     ],
 
-    tests: [{ pattern: "src/**/*.test.js*", load: true }],
+    tests: [{ pattern: 'test/**/*.test.ts?(x)', load: true }],
 
     compilers: {
-      "**/*.js*": wallaby.compilers.babel()
+      '**/*.ts?(x)': wallaby.compilers.typeScript({
+        module: 'commonjs',
+      }),
     },
+
+    moduleDirectories: ['node_modules'],
 
     env: {
-      type: "node",
-      runner: "node"
+      type: 'node',
+      runner: 'node',
     },
 
-    testFramework: "jest"
+    testFramework: 'jest',
   };
 };
